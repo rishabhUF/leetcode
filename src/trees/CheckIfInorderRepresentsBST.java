6ee19f9ed12bb4fbec3d75dbc3d5736c66da7c92
3709611 <rishabhsharma@ufl.edu>
2019-04-01 22:52:46 -0400

    Check if an array represents Inorder of Binary Search tree or not

---
+++ src/trees/CheckIfInorderRepresentsBST.java
@@ -0,0 +1,28 @@
+package trees;
+
+public class CheckIfInorderRepresentsBST {
+
+	static boolean isInorder(int[] arr, int n) {
+		if (n == 0 || n == 1) {
+			return true;
+		}
+
+		for (int i = 1; i < n; i++)
+		{
+			if (arr[i - 1] > arr[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {19, 23, 25, 30, 45};
+		int n = arr.length;
+		if (isInorder(arr, n)) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("Non");
+		}
+	}
+}
